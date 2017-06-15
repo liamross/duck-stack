608744f76bd8522e6907b9830797fb4d98cf36b3
Liam Ross <liamross@telus.net>
2017-06-15 11:32:25 -0700

    oops, curly braces

--- DucksModule/component.jsx
+++ DucksModule/component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ComponentName = ({ props }) => ({
+const ComponentName = ({ props }) => (
     <div>
       
     </div>
-});
+);
 
 ComponentName.defaultProps = {
   
