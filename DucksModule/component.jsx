efff825a68d07cc7f39f67425e135fbcf8d0fb99
Liam Ross <liamross@telus.net>
2017-06-15 11:20:04 -0700

    modernized code

--- DucksModule/component.jsx
+++ DucksModule/component.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ComponentName = ({ props }) => {
-
-  return (
+const ComponentName = ({ props }) => ({
     <div>
       
     </div>
-  );
-};
+});
 
 ComponentName.defaultProps = {
   
